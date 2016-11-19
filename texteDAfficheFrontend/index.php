e128e878952cca0a7b8ee7c496ad59d5be658ab7
KarlPineau <karl.pineau@ens-lyon.fr>
2016-11-19 17:25:55 +0100

    Ajout des fichiers de bases

---
+++ texteDAfficheFrontend/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <?php include('meta.html'); ?>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="../web/css/main.css" type="text/css" />
+        <link rel="stylesheet" href="../web/font-awesome/css/font-awesome.min.css" type="text/css" />
+
+        <!-- Le fav and touch icons -->
+        <link rel="shortcut icon" href="../web/images/logo-big.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../web/images/logo-big.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../web/images/logo-big.png">
+        <link rel="apple-touch-icon-precomposed" href="../web/images/logo-big.png">
+    </head>
+
+    <body>
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
