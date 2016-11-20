90c593c26eebddd2ac85ead8a2250542c37fef8a
KarlPineau <karl.pineau@ens-lyon.fr>
2016-11-20 08:40:04 +0100

    MAJ Frontend morning

--- texteDAfficheFrontend/index.php
+++ texteDAfficheFrontend/index.php
@@ -64,30 +64,30 @@
         <div id="templateAfficheInfoContainer" class="hidden">
             <div class="row templateAfficheInfoContainerRow">
                 <div class="pull-right" style=""><button class="btn btn-sm btn-default btn-remove-templateAfficheInfoContainerRow"><i class="fa fa-times" aria-hidden="true"></i></button></div>
-                <div class="col-sm-6">
+                <div class="col-sm-6 containerForCartelContainer">
                     <h2 class="templateAfficheInfoContainer-title">Titre film</h2>
                     <div class="list-group">
-                        <dl class="list-group-item dl-horizontal">
+                        <dl class="list-group-item dl-horizontal templateAfficheInfoContainerDl-director">
                             <dt>Réalisateur</dt>
                             <dd class="templateAfficheInfoContainer-director"></dd>
                         </dl>
-                        <dl class="list-group-item dl-horizontal">
+                        <dl class="list-group-item dl-horizontal templateAfficheInfoContainerDl-starring">
                             <dt>Distribution</dt>
                             <dd class="templateAfficheInfoContainer-starring"></dd>
                         </dl>
-                        <dl class="list-group-item dl-horizontal">
+                        <dl class="list-group-item dl-horizontal templateAfficheInfoContainerDl-date">
                             <dt>Date de réalisation</dt>
                             <dd class="templateAfficheInfoContainer-date"></dd>
                         </dl>
-                        <dl class="list-group-item dl-horizontal">
+                        <dl class="list-group-item dl-horizontal templateAfficheInfoContainerDl-language">
                             <dt>Langue</dt>
                             <dd class="templateAfficheInfoContainer-language"></dd>
                         </dl>
-                        <dl class="list-group-item dl-horizontal">
+                        <dl class="list-group-item dl-horizontal templateAfficheInfoContainerDl-country">
                             <dt>Pays</dt>
                             <dd class="templateAfficheInfoContainer-country"></dd>
                         </dl>
-                        <dl class="list-group-item dl-horizontal">
+                        <dl class="list-group-item dl-horizontal templateAfficheInfoContainerDl-genre">
                             <dt>Genre</dt>
                             <dd class="templateAfficheInfoContainer-genre"></dd>
                         </dl>
@@ -134,14 +134,14 @@
         <div id="templateIllustrationInfoContainer" class="hidden">
             <div class="row templateIllustrationInfoContainerRow">
                 <div class="pull-right" style=""><button class="btn btn-sm btn-default btn-remove-templateIllustrationInfoContainerRow"><i class="fa fa-times" aria-hidden="true"></i></button></div>
-                <div class="col-sm-6">
+                <div class="col-sm-6 containerForCartelContainer">
                     <h2 class="templateIllustrationInfoContainer-title">Titre film</h2>
                     <div class="list-group">
-                        <dl class="list-group-item dl-horizontal">
+                        <dl class="list-group-item dl-horizontal templateIllustrationInfoContainerDl-author">
                             <dt>Auteur</dt>
                             <dd class="templateIllustrationInfoContainer-author"></dd>
                         </dl>
-                        <dl class="list-group-item dl-horizontal">
+                        <dl class="list-group-item dl-horizontal templateIllustrationInfoContainerDl-publication">
                             <dt>Date de publication</dt>
                             <dd class="templateIllustrationInfoContainer-publication"></dd>
                         </dl>
@@ -210,17 +210,63 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-        <script src="web/js/movieTObook.js"></script>
-        <script src="web/js/bookTOmovie.js"></script>
-        <script>
-            $('#btnBookTOMovie').on('click', function() {
-                $('#btnMovieTOBook').removeClass('active');
-                $(this).addClass('active');
-            });
+        <script type="text/javascript">
+            function initContent() {
+                $('#sectionContent').html('');
+            }
+        </script>
+        <script src="web/js/movieTObook.js" type="text/javascript"></script>
+        <script src="web/js/bookTOmovie.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                var bool = 'book';
 
-            $('#btnMovieTOBook').on('click', function() {
-                $('#btnBookTOMovie').removeClass('active');
-                $(this).addClass('active');
+                $('#btnBookTOMovie').on('click', function() {
+                    $('#btnMovieTOBook').removeClass('active');
+                    $(this).addClass('active');
+                    bool = 'book';
+                    search('*');
+                });
+
+                $('#btnMovieTOBook').on('click', function() {
+                    $('#btnBookTOMovie').removeClass('active');
+                    $(this).addClass('active');
+                    bool = 'movie';
+                    search('*');
+                });
+
+                function search(value) {
+                    if(bool == 'book') {
+                        $.ajax({
+                            dataType: "json",
+                            url: 'data2.json',
+                            success: function (data) {
+                                loadDataBook(data);
+                            },
+                            error: function (error) {
+                                console.log(dump(error));
+                            }
+                        });
+                    }
+                    else if(bool == 'movie') {
+                        $.ajax({
+                            dataType: "json",
+                            url: 'data.json',
+                            success: function (data) {
+                                loadDataMovie(data);
+                            },
+                            error: function (error) {
+                                console.log(dump(error));
+                            }
+                        });
+                    }
+                }
+
+                $('#searchButton').on('click', function() {
+                    search($('#searchInput'));
+                });
+
+                search('*');
             });
         </script>
     </body>
